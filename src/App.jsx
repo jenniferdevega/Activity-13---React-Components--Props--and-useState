482a54da3a83a609d993e3f7496babc3d7d63ccf
jenniferdevega <devega.jennifer.alvarez@gmail.com>
2023-10-31 22:32:56 +0800

    Removed unused

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,17 @@
 import { useState } from 'react';
-import './css/App.css';
+
 import Header from './Header.jsx';
 import Counter from './Counter.jsx';
 import Footer from './Footer.jsx';
-import 'bootstrap/dist/css/bootstrap.css';
+
 import 'bootstrap/dist/js/bootstrap.js';
 
+import 'bootstrap/dist/css/bootstrap.css';
+
 function App() {
-
-  // const [variableName, functionName] = useState();
-
-  // const [name, setName] = useState("Eper");
-
-  // const [counter, setCounter] = useState(0);
+  // NOTE: const [variableName, functionName] = useState();
 
   const [title, setTitle] = useState("Activity 13 - React Components, Props, and useState");
-
   const [copyright, setCopyright] = useState("Copyright 2023 © De Vega, Jennifer. All rights Reserved.");
 
   return (
