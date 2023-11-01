842d337676958543f667ec9dd777263d958d877b
jenniferdevega <devega.jennifer.alvarez@gmail.com>
2023-11-01 19:20:06 +0800

    Cleaner code

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -5,31 +5,30 @@ function Counter(){
 
     const [counter, setCounter] = useState(0);
 
+    //Functions
+    const incrementCounter = ()=>{
+        setCounter(counter + 1);
+    }
+
+    const decrementCounter = () =>{
+        if(counter > 0){
+            setCounter(counter - 1)
+        }
+    }
+
     return(
         <main className="container d-flex justify-content-center mt-5 mb-4">
             <div className="card shadow px-5 py-1 text-center d-flex justify-content-center w-75 mb-5">
                 <div className="card-body">
                     <div className="container">
                         <CountDisplay count={counter} />
-                        <button className="btn btn-info text-light fw-bold mt-3" onClick={
-                            () => {
-                                setCounter(counter + 1)
-                            }
-                        }>
+                        <button className="btn btn-info text-light fw-bold mt-3" onClick={incrementCounter}>
                             + Increment
                         </button>
 
-                        <button className="ms-3 btn btn-info text-light fw-bold mt-3" onClick={
-                            () =>{
-                                if(counter > 0){
-                                    setCounter(counter - 1)
-                                }
-                            }
-                        }>
+                        <button className="ms-3 btn btn-info text-light fw-bold mt-3" onClick={decrementCounter}>
                             - Decrement
                         </button>
-
-
                     </div>
 
                     <div className="mt-3">
@@ -45,7 +44,6 @@ function Counter(){
             </div>
         </main>
     )
-
 }
 
 export default Counter;
